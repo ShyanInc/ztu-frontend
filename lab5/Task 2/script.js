b818acb4151149bb18c9f11e893693bd8aba7f83
Shyan <shyan.inc@gmail.com>
2023-10-03 23:50:07 +0300

    Add Lab 5 Task 2

---
+++ lab5/Task 2/script.js
@@ -0,0 +1,19 @@
+const checkboxes = document.querySelectorAll('input[type="checkbox"]')
+
+const div = document.querySelector('div');
+const emails = [];
+checkboxes.forEach(el => {
+    el.addEventListener('change', function() {
+        if (this.checked) {
+            emails.push(this.value);
+        }
+        else {
+            let index = emails.indexOf(this.value);
+            if (index !== -1) {
+                emails.splice(index, 1);
+            }
+        }
+        div.innerHTML = emails.join('; ')
+    })
+})
+
